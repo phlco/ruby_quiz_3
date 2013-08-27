da516a2dd4e81619e637291b8ae64a2019f25676
Christopher Wright <cmwright33@gmail.com>
2013-08-27 10:23:17 -0400

    stuck on 4

--- quiz.rb
+++ quiz.rb
@@ -21,6 +21,9 @@ QUIZ_TOPICS = ["ruby", "rspec", "testing"]
 # big_diff(array)
 #  [3, 6, 9, 12] => 3
 #  [9, 20, 11, 12] => 8
+def big_dif(array)
+  new_number = array.max - array.sort[-2]
+end
 
 # return the average of the 2 or 3 numbers in the middle of the array
 # (2 if the array has an even number of elements)
@@ -29,12 +32,34 @@ QUIZ_TOPICS = ["ruby", "rspec", "testing"]
 #  [2, 6, 9, 11] => 7.5
 #  [9, 8, 2, 4, 7] => 4.66
 
+def average_median(array)
+  elements = array.count
+  if elements.even?
+    mid_index = (elements/ 2) - 1
+    median = (array[mid_index].to_f + array[mid_index + 1])/2
+  else
+    mid_index = (elements/2) - 1
+    median = (array[mid_index - 1].to_f + array[mid_index] + array[mid_index + 1])/3
+  end
+end
+
+
 # return true if you can split the array into parts
 # where the sum of one portion equals the sum of the other portion.
 # can_balance(array)
 #  [1, 2, 1, 1, 1] => true
 #  [2, 1, 1, 2, 1] => false
 #  [3, 4, 1, 2, 3, 1] => true
+
+def can_balance(array)
+  if array[]
+    return true
+
+  else
+    return false
+  end
+end
+
 
 # Returns the number of times 2 or more of the same number appear sequentially.
 # count_clumps(array)
