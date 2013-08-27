14ca8963c9618331dbc6a19d8291cbb923be5ac8
egong <egong82@gmail.com>
2013-08-27 10:25:27 -0400

    pull request

--- quiz.rb
+++ quiz.rb
@@ -12,7 +12,71 @@
 #      | |\ \       || )
 #     (_/ (_/      ((_/
 
-QUIZ_TOPICS = ["ruby", "rspec", "testing"]
+class Quiz
+
+def new
+  @quiz = Quiz.new
+end
+def topics
+  return ["ruby", "rspec", "testing"]
+end
+
+def love
+  return 'quizzes'
+end
+
+def big_diff(array)
+#comments are all the code i wrote before i got this right, the code
+#that is Commented Out Below DOES NOT WORK. It is there only to show
+#my thought process
+# #find biggest item in array
+# highest = array.max
+# #take out biggest item
+# new_array = array.pop(highest)
+# #take out newest highest in array
+# second = new_array.max
+# return (highest - second)
+a = (array.sort)
+one = a.pop
+two = a.pop
+return (one-two)
+end
+
+def average_median(array)
+  # googled this one, passed for first array in tests but not second
+  #kind of get what its saying but this was toward end of quiz
+  #so not enough time to read
+  #i think its not supposed to be sorted
+  sorted = array.sort
+  len = sorted.length
+  return (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
+  # return the average of the 2 or 3 numbers in the middle of the array
+# (2 if the array has an even number of elements)
+# (3 if the array has an odd number of elements).
+# average_median(array)
+#  [2, 6, 9, 11] => 7.5
+#  [9, 8, 2, 4, 7] => 4.66
+end
+
+def can_balance(array)
+  # return true if you can split the array into parts
+# where the sum of one portion equals the sum of the other portion.
+# can_balance(array)
+#  [1, 2, 1, 1, 1] => true
+#  [2, 1, 1, 2, 1] => false
+#  [3, 4, 1, 2, 3, 1] => true
+end
+
+def count_clumps(array)
+
+# Returns the number of times 2 or more of the same number appear sequentially.
+# count_clumps(array)
+#  [2, 1, 1, 1, 1, 3, 3, 3, 1] => 2
+#  [9, 3, 3, 4, 4, 4, 6, 7, 7, 7] => 3
+end
+
+
+end
 
 # Given an array...
 
