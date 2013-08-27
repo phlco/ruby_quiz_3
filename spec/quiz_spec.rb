b10e97c10ee987b7e2502d33c0c16fdd04b60f22
Christine Coulter <christinetcoulter@gmail.com>
2013-08-27 10:29:01 -0400

    Complete 3/4.

--- spec/quiz_spec.rb
+++ spec/quiz_spec.rb
@@ -11,6 +11,27 @@ describe "a quiz" do
     expect(QUIZ_TOPICS).to include("rspec")
     expect(QUIZ_TOPICS).to include("testing")
   end
+
+  it "should find the largest number and second largest number" do
+    expect(big_diff([3, 6, 9, 12])).to eq(3)
+    expect(big_diff([9, 20, 11, 12])).to eq(8)
+  end
+
+  it "should return the average of the 2 or 3 numbers in the middle of the array" do
+    expect(average_median([2, 6, 9, 11])).to eq(7.5)
+    expect(average_median([9, 8, 2, 4, 7])).to eq(4.66)
+  end
+
+  # it "returns true if you can split the array into parts where the sum of one portion equals the sum of the other portion" do
+  #   expect(can_balance([1, 2, 1, 1, 1])).to eq(true)
+  #   expect(can_balance([2, 1, 1, 2, 1])).to eq(false)
+  #   expect(can_balance([3, 4, 1, 2, 3, 1])).to eq(true)
+  # end
+
+  it "returns the number of times 2 or more of the same number appear sequentially" do
+    expect(count_clumps([2, 1, 1, 1, 1, 3, 3, 3, 1])).to eq(2)
+    expect(count_clumps([9, 3, 3, 4, 4, 4, 6, 7, 7, 7])).to eq(3)
+  end
 end
 
 # more in quiz.rb!
